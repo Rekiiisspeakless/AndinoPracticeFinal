8aad0ead8cf1e155a0cdaa1b3b75902638cf519f
jerry_000 <jerrysupertw@gmail.com>
2017-06-17 18:11:50 +0800

    update testcase

--- myTestcase.c
+++ myTestcase.c
@@ -2,16 +2,23 @@ int main() {
 	int a;
 	int b = 0;
 	int c = 1 / 2 + (3 % (3 - 1) * 5);
+	//c = 5
 	a = c;
-	int d = 10 % (1 + (a * (b + 5) - c) / 2);
+	a = c + 1;
+	// a = 6
+	int d = 14 % (1 + (a * (b + 5) - c) / 2);
+	// d = 1
 	a = a + 100;
 	a = a / 2;
 	a = a - 3;
 	a = a * 2;
 	a = a % 2;
+	//a = 0
 	digitalWrite(13, HIGH);
-	delay(b);
+	// 1
+	delay((a + d) * 1000);
 	digitalWrite(13, LOW);
-	delay(b);
+	// 5
+	delay((b + c) * 1000);
 	return 0;
 }
